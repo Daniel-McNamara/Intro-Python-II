934882b12e373d86de49d8ade4f7bdd11754a321
daniel mcnamara <danmac8407@gmail.com>
2020-07-09 14:41:13 -0500

    rewrote game, added items, fun to play

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,9 @@
+# import files
 from room import Room
 from player import Player
-import textwrap
+from item import Item
+#import modules
+import time
 
 
 # Declare all the rooms
@@ -36,13 +39,101 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+# create items
+
+sword = Item("sword", "A relic from past eras.")
+mirror = Item("mirror", "your face reflects as you stare into it.")
+dagger = Item("dagger", "useful against those that want to eat you")
+chicken = Item("chicken", "Cluck-Cluck")
+coin = Item("coin", "Should be worth a lot!")
+
+
+# Add items to rooms
+room['outside'].items.append(sword)
+room['foyer'].items.append(chicken)
+room['foyer'].items.append(mirror)
+room['overlook'].items.append(dagger)
+room['treasure'].items.append(coin)
+
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player("Daniel", room["outside"])
+new_player = Player(room['outside'])
+
+
+# Initialize Game
+print("> Welcome to the Game. It's fun...")
+print("> You are a thief searching for gold, find the treasure to win the game!")
+print('')
+print(
+    "> Controls: [n] move north [e] move east [s] move south [w] move west [q] quit game")
+print("> Tips! Try to *look* for items, *pickup* items, or *use* items!")
+print('')
+
+# create loop
+while True:
+    # opening statement
+    print('')
+    print(new_player.current_room)
+    print('')
+
+    # check for boss battle
+    if new_player.current_room.name == "Treasure Chamber":
+        print("> Suddenly a Monster emerges from the Darkness!!")
+        print("> You have 15 seconds to decide before certain DOOM!")
+        now = time.time()
+        future = now + 15
+
+    action = input("What do you want to do?  ").split()
+
+    # time check for boss battle
+    if new_player.current_room.name == "Treasure Chamber":
+        if time.time() > future:
+            print("> The Monster eats you")
+            print("> You Died.")
+            break
+
+    # quit game
+    if action[0] == 'q':
+        print('> Thanks for playing.')
+        break
+
+    # movement actions
+    elif action[0] in ['n', 'e', 's', 'w']:
+        print('')
+        new_player.move(action[0])
+        continue
+
+    elif action[0] in ['look', 'search', 'find']:
+        print('')
+        new_player.look_for_items()
+        continue
+
+    elif len(action) > 1:
+        if action[0] in ['pickup', 'take', 'grab']:
+            print('')
+            result = new_player.pickup(action[1])
+            if result == False:
+                print(f"> Couldn't pickup {action[1]}")
+            continue
+        elif action[0] in ['use']:
+            print('')
+            result = new_player.use_item(action[1])
+            if result == True:
+                print("You won! Congratulations!")
+                break
+            continue
+        else:
+            print('')
+            print("> Doesn't look like you can do that...")
+
+    else:
+        print('')
+        print('> Action not recognized. Try again.')
+
 
 # Write a loop that:
 #
@@ -54,21 +145,3 @@ player = Player("Daniel", room["outside"])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-
-
-print(player.name)
-
-user_is_playing = True
-while user_is_playing:
-    print(player.current_room.name)
-
-    for line in textwrap.wrap(player.current_room.description, 40):
-        print(line)    
-
-    user_input = input("Which direction would you like to go? (n/e/s/w): ")
-    if user_input in ["n", "e", "s","w"]:
-        player.move(user_input)
-
-    elif user_input == 'q':
-        print("You have quit the game, thank you for playing!")
-        user_is_playing = False
---
+++ src/item.py
@@ -0,0 +1,14 @@
+  
+import textwrap
+
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return textwrap.fill(f"{self.name}, {self.description}", width=60)
+
+    # def __repr__(self):
+    #     return textwrap.fill(f"{self.name}, {self.description}", width=60)
